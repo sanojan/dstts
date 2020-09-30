9c0d4d0a4c1d0e33f3ad43f950b04b112297495c
vskisho <vskisho@live.com>
2020-09-30 14:59:16 +0530

    Signup Page Front End

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,16 +13,19 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap Core Css -->
-    <link href="../../plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="{{asset('plugins/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
 
     <!-- Waves Effect Css -->
-    <link href="../../plugins/node-waves/waves.css" rel="stylesheet" />
-
+    <link href="{{asset('plugins/node-waves/waves.css')}}" rel="stylesheet" />
+    
+    <!-- date time picker -->
+    <link href="{{asset('plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css')}}" rel="stylesheet" />
+    
     <!-- Animation Css -->
-    <link href="../../plugins/animate-css/animate.css" rel="stylesheet" />
+    <link href="{{asset('plugins/animate-css/animate.css')}}" rel="stylesheet" />
 
     <!-- Custom Css -->
-    <link href="../../css/style.css" rel="stylesheet">
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
 </head>
 
 <body class="signup-page">
@@ -37,60 +40,200 @@
                     @csrf
                     <div class="msg">Register a new membership</div>
                     <div class="row clearfix">
-                        <div class="col-sm-6">
-                    <div class="input-group">
-                        <span class="input-group-addon">
-                            <i class="material-icons">person</i>
-                        </span>
-                        <div class="form-line">
-                            <input type="text" class="form-control" name="fullname" placeholder="Fullname" required autofocus>
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">person</i>
+                                </span>
+                                <div class="form-line">
+                                    <input type="text" class="form-control" name="fullname" placeholder="Fullname" required autofocus>
+                                </div>
+                            </div> 
                         </div>
-                    </div> 
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">people</i>
+                                </span>
+                                <div class="form-line">
+                                <select class="form-control" id="gender" name="gender">
+                                    <option value="">Gender</option>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                                </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">event</i>
+                                </span>
+                                <div class="form-line">
+                                    <input type="date" class="form-control" name="dob" placeholder="Date of birth" required>
+                                </div>
+                            </div> 
+                        </div>
+
                     </div>
-                    <div class="col-sm-6">
-                    <div class="input-group">
-                        <span class="input-group-addon">
-                            <i class="material-icons">person</i>
-                        </span>
-                        <div class="form-line">
-                            <input type="text" class="form-control" name="namewinitial" placeholder="Name with initial" required autofocus>
+                    <div class="row clearfix">
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">card_membership</i>
+                                </span>
+                                <div class="form-line">
+                                <input type="text" class="form-control" placeholder="NIC NO" name="nic">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">email</i>
+                                </span>
+                                <div class="form-line">
+                                <input type="text" class="form-control" placeholder="E-Mail Address" name="email">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">phone_iphone</i>
+                                </span>
+                                <div class="form-line">
+                                <input type="text" class="form-control" placeholder="Mobile No" name="mobno">
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    </div>
-                    </div>
-                    <div class="input-group">
-                        <span class="input-group-addon">
-                            <i class="material-icons">email</i>
-                        </span>
-                        <div class="form-line">
-                            <input type="email" class="form-control" name="email" placeholder="Email Address" required>
+
+                    <div class="row clearfix">
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">work</i>
+                                </span>
+                                <div class="form-line">
+                                <input type="text" class="form-control" placeholder="Designation" name="designation">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">business_center</i>
+                                </span>
+                                <div class="form-line">
+                                <select class="form-control" id="sname" name="sname">
+                                    <option value="">Service</option>
+                                </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">people_outline</i>
+                                </span>
+                                <div class="form-line">
+                                <select class="form-control" id="exampleFormControlSelect1" name="sclass">
+                                    <option value="">Class</option>
+                                </select>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="input-group">
-                        <span class="input-group-addon">
-                            <i class="material-icons">lock</i>
-                        </span>
-                        <div class="form-line">
-                            <input type="password" class="form-control" name="password" minlength="6" placeholder="Password" required>
+
+                    <div class="row clearfix">
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">location_city</i>
+                                </span>
+                                <div class="form-line">
+                                <select class="form-control" id="wplace_type" name="wplace_type">
+                                    <option value="">Work Place Type</option>
+                                </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">location_city</i>
+                                </span>
+                                <div class="form-line">
+                                <select class="form-control" id="wplace_name" name="wplace_name">
+                                    <option value="">Work Place Name</option>
+                                </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">share</i>
+                                </span>
+                                <div class="form-line">
+                                <select class="form-control" id="branch" name="branch">
+                                    <option value="">Branch</option>
+                                </select>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <div class="row clearfix">
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">insert_drive_file</i>
+                                </span>
+                                <div class="form-line">
+                                <input type="text" class="form-control" placeholder="Subject" name="subject">
+                                </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="material-icons">supervisor_account</i>
+                                    </span>
+                                    <div class="form-line">
+                                    <select class="form-control" id="user_type" name="user_type">
+                                        <option value="">User Type</option>
+                                    </select>
+                                    </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">lock</i>
+                                </span>
+                                <div class="form-line">
+                                    <input type="password" class="form-control" name="confirm" minlength="6" placeholder="Confirm Password" required>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="input-group">
-                        <span class="input-group-addon">
-                            <i class="material-icons">lock</i>
-                        </span>
-                        <div class="form-line">
-                            <input type="password" class="form-control" name="confirm" minlength="6" placeholder="Confirm Password" required>
-                        </div>
-                    </div>
+
+
+                    
                     <div class="form-group">
                         <input type="checkbox" name="terms" id="terms" class="filled-in chk-col-pink">
                         <label for="terms">I read and agree to the <a href="javascript:void(0);">terms of usage</a>.</label>
                     </div>
-
+                
                     <button class="btn btn-block btn-lg bg-pink waves-effect" type="submit">SIGN UP</button>
-
+                    
                     <div class="m-t-25 m-b--5 align-center">
                         <a href="{{route('login')}}">You already have a membership?</a>
+                    </div>
                     </div>
                 </form>
             </div>
@@ -98,20 +241,24 @@
     </div>
 
     <!-- Jquery Core Js -->
-    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
 
     <!-- Bootstrap Core Js -->
-    <script src="../../plugins/bootstrap/js/bootstrap.js"></script>
+    <script src="{{asset('plugins/bootstrap/js/bootstrap.js')}}"></script>
 
     <!-- Waves Effect Plugin Js -->
-    <script src="../../plugins/node-waves/waves.js"></script>
+    <script src="{{asset('plugins/node-waves/waves.js')}}"></script>
 
     <!-- Validation Plugin Js -->
-    <script src="../../plugins/jquery-validation/jquery.validate.js"></script>
-
+    <script src="{{asset('plugins/jquery-validation/jquery.validate.js')}}"></script>
+    
+    <!-- date time picker Js -->
+    <script src="{{asset('plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js')}}"></script>
+    
     <!-- Custom Js -->
-    <script src="../../js/admin.js"></script>
-    <script src="../../js/pages/examples/sign-up.js"></script>
+    <script src="{{asset('js/admin.js')}}"></script>
+    
+    <script src="{{asset('js/pages/examples/sign-up.js')}}"></script>
 </body>
 
 </html>
