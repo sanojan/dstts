4460fe8dbc06b51c630ae81a87cabd9f725e1dcd
sanojan <starnetsweb@gmail.com>
2021-05-27 10:10:28 +0530

    bug fixed

--- database/seeds/WorkplaceSeeder.php
+++ database/seeds/WorkplaceSeeder.php
@@ -16,108 +16,129 @@ class WorkplaceSeeder extends Seeder
             array(
             'workplace_type_id' => '1',
             'name' => 'Ampara-District Secretariat',
-            'short_code' => 'AM'
+            'short_code' => 'AM',
+            'contact_no' => '0632222233'
 
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Ampara-Divisional Secretariat',
-                'short_code' => 'AMP'
+                'short_code' => 'AMP',
+                'contact_no' => '0632223435'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Alayadivembu-Divisional Secretariat',
-                'short_code' => 'ALV'
+                'short_code' => 'ALV',
+                'contact_no' => '0672277436'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Uhana-Divisional Secretariat',
-                'short_code' => 'DSU'
+                'short_code' => 'DSU',
+                'contact_no' => '0632250426'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Mahaoya-Divisional Secretariat',
-                'short_code' => 'DSM'
+                'short_code' => 'DSM',
+                'contact_no' => '0632244006'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Padiyathalawa-Divisional Secretariat',
-                'short_code' => 'DSP'
+                'short_code' => 'DSP',
+                'contact_no' => '0632246035'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Dehiaththakandiya-Divisional Secretariat',
-                'short_code' => 'DAK'
+                'short_code' => 'DAK',
+                'contact_no' => '0272250197'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Damana-Divisional Secretariat',
-                'short_code' => 'DSD'
+                'short_code' => 'DSD',
+                'contact_no' => '0632240034'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Lahugala-Divisional Secretariat',
-                'short_code' => 'LAH'
+                'short_code' => 'LAH',
+                'contact_no' => '0632051851'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Irakkamam-Divisional Secretariat',
-                'short_code' => 'IRK'
+                'short_code' => 'IRK',
+                'contact_no' => '0632223189'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Sammanthurai-Divisional Secretariat',
-                'short_code' => 'SMT'
+                'short_code' => 'SMT',
+                'contact_no' => '0672260236'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Karathivu-Divisional Secretariat',
-                'short_code' => 'KTV'
+                'short_code' => 'KTV',
+                'contact_no' => '0672222036'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Sainthamaruthu-Divisional Secretariat',
-                'short_code' => 'SAM'
+                'short_code' => 'SAM',
+                'contact_no' => '0672226288'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Ninthavur-Divisional Secretariat',
-                'short_code' => 'NTR'
+                'short_code' => 'NTR',
+                'contact_no' => '0672050334'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Addalachchenai-Divisional Secretariat',
-                'short_code' => 'ADD'
+                'short_code' => 'ADD',
+                'contact_no' => '0672055336'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Navithanveli-Divisional Secretariat',
-                'short_code' => 'NAV'
+                'short_code' => 'NAV',
+                'contact_no' => '0672223256'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Akkaraipaththu-Divisional Secretariat',
-                'short_code' => 'AKP'
+                'short_code' => 'AKP',
+                'contact_no' => '0672277236'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Thirukkovil-Divisional Secretariat',
-                'short_code' => 'TKV'
+                'short_code' => 'TKV',
+                'contact_no' => ''
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Pothuvil-Divisional Secretariat',
-                'short_code' => 'PVL'
+                'short_code' => 'PVL',
+                'contact_no' => '0632248007'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Kalmunai-Divisional Secretariat',
-                'short_code' => 'KAL'
+                'short_code' => 'KAL',
+                'contact_no' => '0672229236'
             ),
             array(
                 'workplace_type_id' => '2',
                 'name' => 'Kalmunai(Tamil)-Divisional Secretariat',
-                'short_code' => 'KMN'
+                'short_code' => 'KMN',
+                'contact_no' => '0672229599'
             ),
         ));
     }
